d90dc516bf7b93d546f720198da0d2709e0315b4
gasser galal <fdxdfc82@gmail.com>
2018-09-14 20:20:42 +0200

    first commit

---
+++ c1.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std ; 
+int main () 
+{
+	int num ; 
+	cout <<"enter your num : " ; 
+	cin >> num ; 
+	
+
+	if ( num >= 0 ) 
+		cout << " your number is positive \n "; 
+	else 
+		cout << " your number is negative \n " ; 
+}
